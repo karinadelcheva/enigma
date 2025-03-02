1d73930726042f4a374307a7ef4c36cf84480ee8
Karina Delcheva <karina.delcheva@ft.com>
2025-03-02 22:10:28 +0200

    Changes to enigma

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -61,18 +61,14 @@ class Notch:
 
 
 class Rotor:
-    def __init__(self, name, location=0, ring_setting=0, initial_position="A"):
+    def __init__(self, name, location=0, ring_setting=1, initial_position="A"):
         self.mapping = mappings[name]
         self.name = name
         self.location = location
-        self.ring_setting = ring_setting
+        self.ring_setting = ring_setting - 1
         self.notch = Notch(notch_map[name]) if name in notch_map else None
         self.position: int = string.ascii_uppercase.index(initial_position)
         self.initial_position = initial_position
-        print('initial position', self.position)
-        print('ring_setting', self.ring_setting)
-        # print('notch', self.notch.position)
-        print('self.mapping', self.mapping)
 
         self.next_rotor = None
         self.prev_rotor = None
@@ -85,22 +81,15 @@ class Rotor:
         # Signal enters pin side (right) and exits contact side (left)
         # 1. Find which pin the signal enters on
         pin_pos = string.ascii_uppercase.index(character)
-        print('inside rotor ', self.name, 'character ', character)
-        print('character position ', pin_pos)
+
 
         # 2. Apply position offset
-        pin_pos = (pin_pos + self.position) % 25
-        print('after position offset', pin_pos)
-
+        pin_pos = (pin_pos + self.position) % ALPHABET_SIZE
         # 3. Apply ring setting offset (opposite direction of position)
-        pin_pos = (pin_pos - self.ring_setting) % 25
-        print('after ring setting offset ', pin_pos)
+        # pin_pos = (pin_pos - self.ring_setting) % 25
         # 2. Follow internal wiring to contact
         contact_char = self.mapping[pin_pos]
-        print('contact_char', contact_char)
-        # # 3. Pass to next rotor if exists
-        # if self.next_rotor:
-        #     return self.next_rotor.encode_right_to_left(contact_char)
+
         return contact_char
 
     def encode_left_to_right(self, character):
@@ -111,13 +100,10 @@ class Rotor:
         # 2. Follow internal wiring backwards to pin
         pin_char = string.ascii_uppercase[contact_pos]
 
-        # 3. Pass to previous rotor if exists
-        if self.prev_rotor:
-            return self.prev_rotor.encode_left_to_right(pin_char)
         return pin_char
 
     def rotate(self):
-        self.position += 1
+        self.position =( self.position + 1 )% ALPHABET_SIZE
 
         if self.has_notch and self.is_at_notch and self.next_rotor:
             self.next_rotor.rotate()
@@ -142,7 +128,7 @@ class Rotor:
 
 
 
-def rotor_from_name(name, location: int = 0, ring_setting: int = 0, initial_position: string = "A"):
+def rotor_from_name(name, location: int = 0, ring_setting: int = 1, initial_position: string = "A"):
     return Rotor(name, location, ring_setting, initial_position)
 
 
@@ -158,11 +144,11 @@ class Enigma:
             ring_setting = [1, 1, 1]
 
         self.rotors = []
-        self.positions = initial_positions
+        self.positions = "".join(reversed(initial_positions))
         self.reflector = reflector
         self.init_rotors(rotor_sequence, ring_setting)
         self.connect_rotors()
-
+        print(f"Rotors set to {[[rotor.position, rotor.name] for rotor in self.rotors]}.")
         self.plugboard = Plugboard()
 
     @property
@@ -175,24 +161,12 @@ class Enigma:
 
     def init_rotors(self, rotor_sequence, ring_setting):
         for index, rotor_name in enumerate(reversed(rotor_sequence)):
-            print('initializing rotor: ', rotor_name, index, ring_setting[index], self.positions[index])
             self.rotors.append(rotor_from_name(rotor_name, index, ring_setting[index], self.positions[index]))
         self.rotors.append(rotor_from_name(self.reflector))  # reflector goes at the end
 
-        logging.info(f"Rotors set to {rotor_sequence}. Reflector set to {self.reflector}.")
-
     @property
     def rotor_positions(self) -> tuple[Any, ...]:
-        """
-        Get the current positions of all three rotors.
-
-        Returns:
-            tuple[int, int, int]: Current positions of the left, middle, and right rotors
-        """
         return tuple(rotor.position for rotor in self.rotors)
-
-    def get_rotor_positions(self):
-        return self.rotor_positions
 
     def connect_rotors(self):
         """Connect each rotor to its neighboring rotors in the sequence,
@@ -200,29 +174,38 @@ class Enigma:
         total_rotors = len(self.rotors)
 
         for position, rotor in enumerate(self.rotors):
-            rotor.left_rotor = self.rotors[position + 1] if position < total_rotors - 1 else None
-            rotor.right_rotor = self.rotors[position - 1] if position > 0 else None
+            if position == 0:
+                prev_rotor = None
+            else:
+                prev_rotor = self.rotors[position - 1]
+            if position == len(self.rotors) - 1:
+                next_rotor = None
+            else:
+                next_rotor = self.rotors[position + 1]
+            rotor.connect(prev_rotor, next_rotor)
 
     def encode_character(self, character: str):
-        """Encode a character using our Enigma machine.
+        """Encode a character using Enigma machine.
         """
         character = character.upper()
         Enigma.__validate_character(character)
         character = self.plugboard.encode(character)
         self.__rotate()
+        print(f"Rotated to {[[rotor.position, rotor.name] for rotor in self.rotors]}.")
 
         # pass characters
         for rotor in self.rotors:
             character = rotor.encode_right_to_left(character)
-
+            print(f"Rotor {rotor.name} Encryption: {character}")
         for rotor in reversed(self.rotors[0:-1]):
             character = rotor.encode_left_to_right(character)
+            print(f"Rotor {rotor.name} Encryption: {character}")
 
-        # calculate offset between last rotor on right and static ring
+        # # calculate offset between last rotor on right and static ring
         inx = self.input_ring.mapping.index(character)
-        first_rotor_offset = self.rotors[0].get_relative_position % ALPHABET_SIZE
-        character = self.input_ring.mapping[(inx - first_rotor_offset) % ALPHABET_SIZE]
-
+        # first_rotor_offset = self.rotors[0].get_relative_position % ALPHABET_SIZE
+        # character = self.input_ring.mapping[(inx - first_rotor_offset) % ALPHABET_SIZE]
+        character = self.input_ring.mapping[inx]
         # swap the character again if connected in the plugboard by lead.
         return self.plugboard.encode(character)
 
@@ -254,4 +237,73 @@ class Enigma:
 
 if __name__ == "__main__":
     # You can use this section to write tests and demonstrations of your enigma code.
-    pass
+    enigma2 = Enigma(rotor_sequence=["I", "II", "III"], reflector="B", ring_setting=[1, 1, 1], initial_positions="AAZ")
+    print(enigma2.encode_character("A"))
+    enigma2 = Enigma(rotor_sequence=["I", "II", "III"], reflector="B", ring_setting=[1, 1, 1], initial_positions="AAA")
+
+    assert enigma2.encode_character("A") == "B"  # Expected output: B
+
+#
+# Keyboard Input: A
+# Rotors Position: AAB
+# Plugboard Encryption: A
+# Wheel 3 Encryption: C
+# Wheel 2 Encryption: D
+# Wheel 1 Encryption: F
+# Reflector Encryption: S
+# Wheel 1 Encryption: S
+# Wheel 2 Encryption: E
+# Wheel 3 Encryption: B
+# Plugboard Encryption: B
+# Output (Lampboard): B
+# -----------------------------
+#
+# Rotor III Encryption: B
+# Rotor II Encryption: A
+# Rotor I Encryption: C
+# Rotor B Encryption: U
+# Rotor I Encryption: R
+# Rotor II Encryption: G
+# Rotor III Encryption: S
+
+
+# Keyboard Input: A
+# Rotors Position: AAA
+# Plugboard Encryption: A
+# Wheel 3 Encryption: B
+# Wheel 2 Encryption: J
+# Wheel 1 Encryption: Z
+# Reflector Encryption: T
+# Wheel 1 Encryption: L
+# Wheel 2 Encryption: K
+# Wheel 3 Encryption: U
+# Plugboard Encryption: U
+# Output (Lampboard): U
+
+#
+# Keyboard Input: F
+# Rotors Position: AAA
+# Plugboard Encryption: F
+# Wheel 3 Encryption: L
+# Wheel 2 Encryption: H
+# Wheel 1 Encryption: Q
+# Reflector Encryption: E
+# Wheel 1 Encryption: A
+# Wheel 2 Encryption: A
+# Wheel 3 Encryption: T
+# Plugboard Encryption: T
+# Output (Lampboard): T
+# -----------------------------
+
+# Keyboard Input: F
+# Rotors Position: AAB
+# Plugboard Encryption: F
+# Wheel 3 Encryption: B
+# Wheel 2 Encryption: J
+# Wheel 1 Encryption: Z
+# Reflector Encryption: T
+# Wheel 1 Encryption: L
+# Wheel 2 Encryption: K
+# Wheel 3 Encryption: E
+# Plugboard Encryption: E
+# Output (Lampboard): E
